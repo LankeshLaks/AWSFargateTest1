3684f5bc408908c5c5e3c2a988f4137274b11c4b
User <User@DESKTOP-EQNCL51>
2024-04-06 21:40:36 +0530

    Add project files.

---
+++ Controllers/UserDetails.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AWSFTest1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetails : ControllerBase
+    {
+        // GET: api/<UserDetails>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "Laks1", "Laks2" });
+        }
+        
+    }
+}
